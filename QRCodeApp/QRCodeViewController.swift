9dfbfbb92adaea8243c0e62b3d6f7e7c05df3bf6
saminshakya <saminshakya27@gmail.com>
2023-09-17 10:34:42 +0545

    starter project

---
+++ QRCodeApp/QRCodeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeApp
+//
+//  Created by Farukh IQBAL on 21/12/2020.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: - Navigation
+    
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ QRCodeApp/QRScannerController.swift
@@ -0,0 +1,20 @@
+//
+//  QRScannerViewController.swift
+//  QRCodeApp
+//
+//  Created by Farukh IQBAL on 21/12/2020.
+//
+
+import UIKit
+
+class QRScannerController: UIViewController {
+    
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var topBar: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
