8ed0c11551d059d19284959075be22d4b94579a2
saminshakya <saminshakya27@gmail.com>
2023-09-19 11:11:26 +0545

    check metadata object array update status labels

--- QRCodeApp/QRScannerController.swift
+++ QRCodeApp/QRScannerController.swift
@@ -20,6 +20,35 @@ class QRScannerController: UIViewController {
     
     @IBOutlet var messageLabel: UILabel!
     @IBOutlet var topBar: UIView!
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        //check if the metadata Objects array is not nil  and it contains at least one object
+        
+        if metadataObjects.count == 0 {
+            qrcodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        //Get the metadata object
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            //if the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject  = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrcodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+        
+               
+        
+          
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +60,7 @@ class QRScannerController: UIViewController {
         }
         
         do {
-            //get an instance of the AVCaptureDeviceInput class using the previous device object
+            //Get an instance of the AVCaptureDeviceInput class using the previous device object
             let input = try AVCaptureDeviceInput(device: captureDevice)
             
             //Set the input device on the capture session
@@ -41,7 +70,7 @@ class QRScannerController: UIViewController {
             let captureMetadataOutput = AVCaptureMetadataOutput()
             captureSession.addOutput(captureMetadataOutput)
             
-            //set delegate and use the default diapatch queue to execute the call back
+            //Set delegate and use the default diapatch queue to execute the call back
             captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
             
@@ -58,7 +87,16 @@ class QRScannerController: UIViewController {
             view.bringSubviewToFront(messageLabel)
             view.bringSubviewToFront(topBar)
             
-            //Initialize QR Code Frame to highlight th QR code
+            //Initialize QR Code Frame to highlight the QR code
+            qrcodeFrameView = UIView()
+            
+            if let qrcodeFrameView = qrcodeFrameView {
+                qrcodeFrameView.layer.borderColor = UIColor.yellow.cgColor
+                qrcodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrcodeFrameView)
+                view.bringSubviewToFront(qrcodeFrameView)
+            }
+            
             
             
             
